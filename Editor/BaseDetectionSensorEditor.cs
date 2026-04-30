0d5d097c7ac15397a518e68fa6893dd480091c25
TITAN\ckilg <ckilgore@gmail.com>
2026-04-30 12:51:45 -0500

    Improved component name UX

--- Editor/BaseDetectionSensorEditor.cs
+++ Editor/BaseDetectionSensorEditor.cs
@@ -17,13 +17,14 @@ namespace ToolkitEditor.Sensors
         protected SerializedProperty m_detectionLayers;
         protected SerializedProperty m_queryTrigger;
 
-        private static List<string> s_cachedComponentNames = null;
+		private static List<string> s_cachedDisplayNames = null;
+		private static List<string> s_cachedTypeNames = null;
 
-        #endregion
+		#endregion
 
-        #region Methods
+		#region Methods
 
-        protected override void OnEnable()
+		protected override void OnEnable()
         {
             base.OnEnable();
             m_detectionMode = serializedObject.FindProperty("m_detectionMode");
@@ -32,10 +33,11 @@ namespace ToolkitEditor.Sensors
             m_queryTrigger = serializedObject.FindProperty("queryTrigger");
         }
 
-        private void OnDisable()
+        protected override void OnDisable()
         {
-            s_cachedComponentNames = null;
-        }
+            s_cachedDisplayNames = null;
+			s_cachedTypeNames = null;
+		}
 
         protected override void OnCustomInspectorGUI()
         {
@@ -56,25 +58,33 @@ namespace ToolkitEditor.Sensors
             EditorGUILayout.PropertyField(m_detectionMode);
             if ((DetectionMode)m_detectionMode.intValue == DetectionMode.Custom)
             {
-                if (s_cachedComponentNames == null)
+                if (s_cachedDisplayNames == null)
                 {
-                    s_cachedComponentNames = System.AppDomain.CurrentDomain.GetAssemblies()
-                        .SelectMany(assembly => assembly.GetTypes())
-                        .Where(type => type.IsClass && type.IsSubclassOf(typeof(Component)))
-                        .Select(x => x.Name)
-                        .OrderBy(x => x).ToList();
-                }
+                    s_cachedDisplayNames = new();
+                    s_cachedTypeNames = new();
+
+					foreach (var p in from assembly in System.AppDomain.CurrentDomain.GetAssemblies()
+									  let assemblyName = assembly.GetName().Name
+									  from type in assembly.GetTypes()
+									  where type.IsClass && type.IsSubclassOf(typeof(Component))
+									  orderby assemblyName, type.Name
+                                      select new { displayName = $"{assemblyName}/{type.Name}", name = type.Name })
+                    {
+                        s_cachedDisplayNames.Add(p.displayName);
+                        s_cachedTypeNames.Add(p.name);
+                    }
+				}
 
                 int index = 0;
                 if (!string.IsNullOrWhiteSpace(m_componentName.stringValue))
                 {
-                    index = s_cachedComponentNames.IndexOf(m_componentName.stringValue);
+                    index = s_cachedTypeNames.IndexOf(m_componentName.stringValue);
                 }
 
                 if (index >= 0)
                 {
-                    index = EditorGUILayout.Popup(m_componentName.displayName, index, s_cachedComponentNames.ToArray());
-                    m_componentName.stringValue = s_cachedComponentNames[index];
+                    index = EditorGUILayout.Popup(m_componentName.displayName, index, s_cachedDisplayNames.ToArray());
+                    m_componentName.stringValue = s_cachedTypeNames[index];
                 }
                 else
                 {
